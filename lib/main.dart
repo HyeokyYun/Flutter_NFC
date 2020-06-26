4666260e497cd53cbcd8dcf2085486f695c690eb
YUNHANKYU <bhgt4563@gmail.com>
2020-06-26 14:45:05 +0900

    [UPDATE] add nfc code

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,88 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:nfc_in_flutter/nfc_in_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  title: "NFC example project",
+  home: NFCReader()),
+);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class NFCReader extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _NFCReaderState createState() => _NFCReaderState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _NFCReaderState extends State {
+  bool _supportsNFC = false;
+  bool _reading = false;
+  StreamSubscription<NDEFMessage> _stream;
+  String text ="default" ;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    // 기기가 NFC 지원하는지 체크
+    // ios 시뮬레이터에서는 아직 지원 안함.
+    // android 에뮬레이터에서 돌리려면 추가 옵션 필요
+    NFC.isNDEFSupported
+        .then((bool isSupported) {
+      setState(() {
+        _supportsNFC = isSupported;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (!_supportsNFC) {
+      return Scaffold(
+        body: Center(
+          child: RaisedButton(
+            child: const Text("NFC를 지원하지 않는 기기입니다."),
+            onPressed: null,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            RaisedButton(
+                child: Text(_reading ? "인식 중지" : "인식 시작"),
+                onPressed: () {
+                  if (_reading) {
+                    _stream?.cancel();
+                    setState(() {
+                      _reading = false;
+                    });
+                  } else {
+                    setState(() {
+                      _reading = true;
+                      // Start reading using NFC.readNDEF()
+                      _stream = NFC.readNDEF(
+                        once: true,
+                        throwOnUserCancel: false,
+                      ).listen((NDEFMessage message) {
+                        print("read NDEF message: ${message.payload}");
+                        setState(() {
+                          text = message.payload;
+                        });
+                      }, onError: (e) {
+                        // Check error handling guide below
+                      });
+                    });
+                  }
+                }
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            SizedBox(height: 30.0,),
+            Text("메시지 : $text"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
